cc3a77ec6f43a41482ebf47eb23131f0715378d1
ympek <sz@ympek.net>
2022-07-23 15:06:36 +0200

    new kmeans implementation inspired by impl in R studio

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <filesystem>
 #include <cmath>
 #include <thread>
+#include <random>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
@@ -27,6 +28,110 @@
 namespace fs = std::filesystem;
 
 const std::string kmeansInputFilename = "KMEANS_INPUT";
+
+void performClusteringInPlace(
+    std::vector<double>& vectorVH,
+    std::vector<double>& vectorVV,
+    int numClasses) {
+
+  std::string outDir = ".floodsar-cache/kmeans_outputs/" + kmeansInputFilename + "_cl_" + std::to_string(numClasses);
+  fs::create_directory(outDir);
+
+  // yeah lets go
+  int maxiter = 100;
+  int numPoints = vectorVH.size();
+  bool updated = false;
+  double best;
+
+  // randomization
+  std::random_device dev;
+  std::mt19937 rng(dev());
+  std::uniform_int_distribution<std::mt19937::result_type> mydist(0, numPoints); // distribution in range [1, 6]
+
+  std::vector<double> centroidsVH;
+  std::vector<double> centroidsVV;
+
+  std::vector<int> clusterAssignments;
+  clusterAssignments.resize(numPoints, 0);
+
+  // first initialize
+  for (int i = 0; i < numClasses; i++) {
+    auto randPoint = mydist(rng);
+    centroidsVH.push_back(vectorVH.at(randPoint));
+    centroidsVV.push_back(vectorVV.at(randPoint));
+  }
+
+  for (int iter = 0; iter < maxiter; iter++) {
+    std::cout << "Iter " << iter << "/" << maxiter << "\n";
+    updated = false;
+    for (int i = 0; i < numPoints; i++) {
+      /* find nearest centre for each point */
+      best = std::numeric_limits<double>::max(); // positive infinity...
+      int newClusterNumber = 0;
+      for (int j = 0; j < numClasses; j++) {
+        double tmpVH = vectorVH.at(i) - centroidsVH.at(j);
+        tmpVH *= tmpVH;
+
+        double tmpVV = vectorVV.at(i) - centroidsVV.at(j);
+        tmpVV *= tmpVV;
+
+        double sum = tmpVH + tmpVV;
+
+        if (sum < best) {
+          best = sum;
+          newClusterNumber = j + 1;
+        }
+      }
+      if (clusterAssignments[i] != newClusterNumber) {
+        updated = true;
+        clusterAssignments[i] = newClusterNumber;
+      }
+    }
+
+    // przeszlismy kazdy punkt. teraz sprawdzmy czy byly update y
+    if (!updated) break;
+
+    // recalculate centroids.
+    for (int i = 0; i < numClasses; i++) {
+      centroidsVH[i] = 0.0;
+      centroidsVV[i] = 0.0;
+    }
+
+    std::vector<int> counts;
+    counts.resize(numClasses, 0);
+
+    for (int i = 0; i < numPoints; i++) {
+      auto centroidIndex = clusterAssignments[i] - 1;
+      centroidsVH[centroidIndex] += vectorVH[i];
+      centroidsVV[centroidIndex] += vectorVV[i];
+      counts[centroidIndex]++;
+    }
+
+    for (int i = 0; i < numClasses; i++) {
+      centroidsVH[i] /= counts[i];
+      centroidsVV[i] /= counts[i];
+    }
+  }
+
+  // dump result.
+  std::cout << "Finished clustering. Dump result.";
+  const std::string clustersPath = outDir + "/" + std::to_string(numClasses) + "-clusters.txt";
+  const std::string pointsPath = outDir + "/" + std::to_string(numClasses) + "-points.txt";
+
+  std::ofstream ofsClusters;
+  ofsClusters.open(clustersPath, std::ofstream::out);
+
+  for (int i = 0; i < numClasses; i++) {
+    ofsClusters << centroidsVH[i] << " " << centroidsVV[i] << "\n";
+  }
+
+  std::ofstream ofsPoints;
+  ofsPoints.open(pointsPath, std::ofstream::out);
+
+  for (int i = 0; i < numPoints; i++) {
+    ofsPoints << clusterAssignments[i] << "\n";
+  }
+}
 
 void performClusteringViaKMeansBinary(std::string inputFilename, int numClasses) {
   std::string outDir = ".floodsar-cache/kmeans_outputs/" + inputFilename + "_cl_" + std::to_string(numClasses);
@@ -474,6 +579,10 @@ int main(int argc, char** argv)
     for (auto& polarization : polarizations) {
       std::vector<double> elevations; // i.e. water levels or discharges
       std::vector<std::string> croppedRasterPaths;
+
+      // new kmeans impl
+      std::vector<double> vhAllPixelValues;
+      std::vector<double> vvAllPixelValues;
 	  
       for (const auto& [day, elevation] : obsElevationsMap) {
         // interesting for us are only dates when we have appropriate picture...
@@ -594,6 +703,8 @@ int main(int argc, char** argv)
 
         for(int i = 0; i < vhPixelValues.size(); i++) {
           ofs << vhPixelValues.at(i) << " " << vvPixelValues.at(i) << "\n";
+          vhAllPixelValues.push_back(vhPixelValues.at(i));
+          vvAllPixelValues.push_back(vvPixelValues.at(i));
         }
       }
 	  
@@ -606,7 +717,8 @@ int main(int argc, char** argv)
 
     if (!userInput.count("skip-clustering")) {
       for (int i : numClassesToTry) {
-        performClusteringViaKMeansBinary(kmeansInputFilename, i);
+        // performClusteringViaKMeansBinary(kmeansInputFilename, i);
+        performClusteringInPlace(vhAllPixelValues, vvAllPixelValues, i);
       }
     }
 
